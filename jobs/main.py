a1e139e60567d4d746009bae15bdebd2f0aafcbc
Shusant Sapkota <shusant.sapkota@pdc01.lisnepal.com.np>
2024-09-04 17:33:47 +0545

    main.py changes

--- jobs/main.py
+++ jobs/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import os
+from confluent_kafka import SerializingProducer
+from datetime import datetime
+import simplejson as json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+LONDON_COORDINATES = {"latitude": 51.5074,"logitude": -0.1278}
+BIRMINGHAM_COORDINATES = {"latitude": 52.4862,"logitude": -1.8904}
+
+# Calculate the movement increment.
+LATITUDE_INCREMENT = (BIRMINGHAM_COORDINATES['latitude']-LONDON_COORDINATES['latitude'])/100
+LONGITUDE_INCREMENT = (BIRMINGHAM_COORDINATES['longitude']-LONDON_COORDINATES['longitude'])/100
+
+#Environment Variable for Configuratio
+KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
+VEHICLE_TOPIC = os.getenv('VEHICLE_TOPIC', 'vehicle_data')
+GPS_TOPIC = os.getenv('GPS_TOPIC', 'gps_data')
+TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
+WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_data')
+EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
+
+start_time = datetime.now()
+start_location = LONDON_COORDINATES.copy()
+
+if __name__ == "__main__"():
+    producer_config = {
+        'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS.
+    }
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
