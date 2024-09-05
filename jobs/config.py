e8663db8854b67cf73b30b61ba6655b4ca248253
Shusant Sapkota <shusant.sapkota@pdc01.lisnepal.com.np>
2024-09-05 14:13:22 +0545

    Configure environment variables and  config.py file

---
+++ jobs/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+configuration = {
+    "AWS_ACCESS_KEY": os.getenv("AWS_ACCESS_KEY"),
+    "AWS_SECRET_KEY": os.getenv("AWS_SECRET_KEY")
+}
+
+print(configuration)
