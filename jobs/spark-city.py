6ad11c5ba9c417e36739ce297414087a8ff59fde
Shusant Sapkota <shusant.sapkota@pdc01.lisnepal.com.np>
2024-09-05 17:37:23 +0545

    Need to test

--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -1,13 +1,14 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructField, StructType, StringType, TimestampType, DoubleType, IntegerType
 from pyspark.sql.functions import from_json, col
+from config import configuration
 
 def main():
     spark = SparkSession.builder.appName("SmartCityStreaming")\
     .config("spark.jars.packages",
-                "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.2",
-                "org.apache.hadoop:hadoop-aws:3.3.1",
-                "com.amazonaws:aws-java-sdk:1.11.469")\
+            "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.2,"
+            "org.apache.hadoop:hadoop-aws:3.3.1,"
+            "com.amazonaws:aws-java-sdk:1.11.469")\
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
     .config("spark.hadoop.fs.s3a.access.key", configuration.get('AWS_ACCESS_KEY'))\
     .config("spark.hadoop.fs.s3a.secret.key", configuration.get('AWS_SECRET_KEY'))\
